227d30bb548a362cee4b4f8765c8051cc0db303c
Nick Beattie <nick@nickbytes.com>
2018-10-17 16:11:11 -0400

    this is like halfway done

--- src/app.tsx
+++ src/app.tsx
@@ -3,22 +3,94 @@ import UrlBarInput from "./components/UrlBarInput";
 import ActiveUrlBar from "./components/ActiveUrlBar";
 import PageContent from "./components/PageContent";
 import styled from "styled-components";
+const mercury = require("mercury-parser")(
+  "PB12LDArVRzLeMogouAolKnimhRDgQcCG1kHCPUy"
+);
 
 const Container = styled.div`
   padding: 10px;
 `;
 
-export class App extends React.Component<undefined, undefined> {
+interface IMercuryResponse {
+  title: string;
+  author: string;
+  date_published: string;
+  dek: string;
+  lead_image_url: string;
+  content: string;
+  next_page_url: string;
+  url: string;
+  domain: string;
+  excerpt: string;
+  word_count: number | null | undefined;
+  direction: string;
+  total_pages: number;
+  rendered_pages: number;
+}
+
+interface AppState {
+  title: string;
+  author: string;
+  content: string;
+  datePublished: string;
+  url: string;
+  wordCount: number | null | undefined;
+}
+
+export class App extends React.Component<{}, AppState> {
+  state = {
+    title: "",
+    author: "",
+    content: "",
+    datePublished: "",
+    url: "",
+    wordCount: null
+  };
   private fetchUrl = () => {
     console.log("fetching url");
+
+    mercury
+      .parse(
+        "https://foreignpolicy.com/2016/07/29/the-end-of-days-is-coming-just-not-to-china-apocalyptic-fiction-movies/"
+      )
+      .then((response: IMercuryResponse) => {
+        console.log(response);
+        this.setState({
+          title: response.title,
+          url: response.url,
+          author: response.author,
+          content: response.content,
+          datePublished: response.date_published,
+          wordCount: response.word_count
+        });
+      })
+      .catch((err: any) => {
+        console.log("Error", err);
+      });
   };
 
   render() {
+    const {
+      title,
+      author,
+      content,
+      datePublished,
+      url,
+      wordCount
+    } = this.state;
     return (
       <Container>
         <UrlBarInput />
+        <button onClick={this.fetchUrl}>search</button>
         <ActiveUrlBar successfulFetch={true} url="example.com" />
-        <PageContent />
+        <PageContent
+          title={title}
+          author={author}
+          content={content}
+          datePublished={datePublished}
+          url={url}
+          wordCount={wordCount}
+        />
       </Container>
     );
   }
--- src/components/PageContent/index.tsx
+++ src/components/PageContent/index.tsx
@@ -5,6 +5,31 @@ const Wrapper = styled.div`
   border: 1px solid #000;
 `;
 
-const PageContent = () => <Wrapper>page content</Wrapper>;
+interface IPageContent {
+  title: string;
+  author: string;
+  content?: string;
+  datePublished: string;
+  url: string;
+  wordCount?: number | null | undefined;
+}
+
+const PageContent = ({
+  title,
+  author,
+  content,
+  datePublished,
+  url,
+  wordCount
+}: IPageContent) => (
+  <Wrapper>
+    <h1>{title}</h1>
+    <h2>{author}</h2>
+    <h3>{datePublished}</h3>
+    <h3>{url}</h3>
+    <h3>{wordCount}</h3>
+    {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
+  </Wrapper>
+);
 
 export default PageContent;
