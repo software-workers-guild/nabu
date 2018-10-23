bf449e7525fac1b4ed4078f5059986d312aefeb3
Nick Beattie <nick@nickbytes.com>
2018-10-22 22:01:00 -0400

    WIP: use node-readability

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import UrlBarInput from "./components/UrlBarInput";
 import PageContent from "./components/PageContent";
-import { IReadabilityProps } from "./types/IReadabilityProps";
+// import { IReadabilityProps } from "./types/IReadabilityProps";
 import { IAppState } from "./types/IAppState";
 import { AppContainer } from "./components/AppContainer";
-// its okay
-// we are going to swap this
-// for node-readability,
-// just chill
-const mercury = require("mercury-parser")(
-  "PB12LDArVRzLeMogouAolKnimhRDgQcCG1kHCPUy"
-);
+// const mercury = require("mercury-parser")(
+//   "KEY_HERE"
+// );
+import { read } from "node-readability";
 
 export class App extends React.Component<{}, IAppState> {
   state = {
@@ -25,22 +22,50 @@ export class App extends React.Component<{}, IAppState> {
   private fetchUrl = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    mercury
-      .parse(this.state.inputValue)
-      .then((response: IReadabilityProps) => {
-        console.log(response);
-        this.setState({
-          title: response.title,
-          url: response.url,
-          author: response.author,
-          content: response.content,
-          datePublished: response.date_published,
-          wordCount: response.word_count
-        });
-      })
-      .catch((err: any) => {
-        console.log("Error", err);
+    // mercury
+    //   .parse(this.state.inputValue)
+    //   .then((response: IReadabilityProps) => {
+    //     console.log(response);
+    //     this.setState({
+    //       title: response.title,
+    //       url: response.url,
+    //       author: response.author,
+    //       content: response.content,
+    //       datePublished: response.date_published,
+    //       wordCount: response.word_count
+    //     });
+    //   })
+    //   .catch((err: any) => {
+    //     console.log("Error", err);
+    //   });
+
+    // WIP
+
+    read(this.state.inputValue, (err: any, article: any, meta: any) => {
+      // Content
+      console.log(article.content);
+      // Title
+      console.log(article.title);
+
+      // HTML Source Code
+      console.log(article.html);
+      // DOM
+      console.log(article.document);
+
+      // Response Object from Request Lib
+      console.log(meta);
+
+      // err
+      console.log(err);
+
+      this.setState({
+        title: article.title,
+        content: article.content
       });
+
+      // Close article to clean up jsdom and prevent leaks
+      article.close();
+    });
   };
 
   private onInputChange = (e: React.FormEvent<HTMLInputElement>): void => {
